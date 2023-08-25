835b9171ae12f0c23e530fe29a0142ca50483e81
mojtabasharafiyeh <61268429+mojtabasharafiyeh@users.noreply.github.com>
2023-08-25 18:41:46 +0330

    work with built in directives and component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'neat tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today ',
+    },
+    {
+      title: 'snowy mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'mountainlover',
+      content: 'here is a picture of snowy mountain',
+    },
+    {
+      title: 'mountain biking',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'biking1222 ',
+      content: 'I did some biking today',
+    },
+  ];
 }
